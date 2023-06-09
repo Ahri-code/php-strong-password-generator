f1f1902dfad32c1152b7b31b807c4f28dc10d251
Edy Galdiolo <edygaldy302@gmail.com>
2023-06-09 17:54:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lenght</title>
+</head>
+<body>
+    <form action="password.php" method="GET">
+        <input type="number" name="lenght">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
